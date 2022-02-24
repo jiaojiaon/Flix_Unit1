af68d01485780df086623f72200fcf524c2494d7
jiaojiaon <jiaojiaon@gmail.com>
2022-02-24 14:44:40 -0500

    upload Flix project

---
+++ Unit1-Flix/Pods/Target Support Files/Pods-Unit1-Movie App-Unit1-Movie AppUITests/Pods-Unit1-Movie App-Unit1-Movie AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Unit1_Movie_App_Unit1_Movie_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Unit1_Movie_App_Unit1_Movie_AppUITests
+@end
